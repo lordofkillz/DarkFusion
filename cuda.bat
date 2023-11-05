9caf2f888003e539a48a60353c8ba6d62daaa1c3
Jason Daniel <83985724+lordofkillz@users.noreply.github.com>
2023-11-05 16:30:08 -0500

    Update cuda.bat

--- cuda.bat
+++ cuda.bat
@@ -21,7 +21,7 @@ IF %ERRORLEVEL% EQU 0 (
 SET "DFPATH=C:\DarkFusion"
 SET "ANACONDAP=%DFPATH%\anaconda"
 SET "OPENCVP=%DFPATH%"
-SET "CUDAPATH=C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.6"
+SET "CUDAPATH=C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.8"
 
 echo Anaconda Path: %ANACONDAP%
 echo OpenCV Path: %OPENCVP%
@@ -65,7 +65,7 @@ echo pathToAnaconda: %pathToAnaconda%
 
 :: Running CMake Commands
 call "%HOMEDRIVE%/Program Files/CMake/bin/cmake.exe" -B"%openCvBuild%" -S"%openCvSource%" -G"%generator%" -DCMAKE_MAKE_PROGRAM="%OPENCVP%/ninja.exe" -DCMAKE_INSTALL_PREFIX="%OPENCVP%/install" -DCMAKE_CONFIGURATION_TYPES=%buildType% -DCMAKE_BUILD_TYPE=%buildType% -DOPENCV_EXTRA_MODULES_PATH="%openCVExtraModules%" ^ -DINSTALL_TESTS=OFF -DINSTALL_C_EXAMPLES=OFF -DBUILD_EXAMPLES=OFF ^ -DWITH_TBB=ON -DWITH_LIBJPEG_TURBO=ON ^
--DBUILD_opencv_world=ON -DWITH_CUDA=ON -DOPENCV_DNN_CUDA=ON -DCUDA_TOOLKIT_ROOT_DIR="%toolkitRoot%" -DCUDA_FAST_MATH=ON -DWITH_CUBLAS=ON -DCUDA_GENERATION=Auto -DWITH_NVCUVID=ON -DWITH_HALIDE=ON -DWITH_OPENGL=ON -DENABLE_FAST_MATH=ON -DWITH_MFX=ON ^ -DBUILD_opencv_python3=ON -DPYTHON3_INCLUDE_DIR=%pathToAnaconda%/include -DPYTHON3_LIBRARY=%pathToAnaconda%/libs/python%pyVer%.lib -DPYTHON3_EXECUTABLE=%pathToAnaconda%/python.exe -DPYTHON3_NUMPY_INCLUDE_DIRS=%pathToAnaconda%/lib/site-packages/numpy/core/include -DPYTHON3_PACKAGES_PATH=%pathToAnaconda%/Lib/site-packages/
+-DBUILD_opencv_world=ON -DWITH_CUDA=ON -DOPENCV_DNN_CUDA=ON -DCUDA_TOOLKIT_ROOT_DIR="%toolkitRoot%" -DCUDA_FAST_MATH=ON -DWITH_CUBLAS=ON -DCUDA_GENERATION=Auto -DWITH_NVCUVID=ON -DWITH_OPENGL=ON -DENABLE_FAST_MATH=ON -DWITH_MFX=ON ^ -DBUILD_opencv_python3=ON -DPYTHON3_INCLUDE_DIR=%pathToAnaconda%/include -DPYTHON3_LIBRARY=%pathToAnaconda%/libs/python%pyVer%.lib -DPYTHON3_EXECUTABLE=%pathToAnaconda%/python.exe -DPYTHON3_NUMPY_INCLUDE_DIRS=%pathToAnaconda%/lib/site-packages/numpy/core/include -DPYTHON3_PACKAGES_PATH=%pathToAnaconda%/Lib/site-packages/
 
 IF %ERRORLEVEL% NEQ 0 (
     ECHO "An error occurred during the build process."
@@ -83,9 +83,5 @@ IF %ERRORLEVEL% NEQ 0 (
 )
 
 echo ULTRADARKFUSION FINISHED!
-
-:: Call Python script to test CUDA
-echo Running CUDA test script
-python "C:\DarkFusion\cuda.py"
 
 PAUSE
